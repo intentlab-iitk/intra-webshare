8fb61a30c12b9aa859c481e93a58c5361c12d15e
Mounika Musugu <mounika30m@gmail.com>
2024-07-13 03:50:04 +0530

    code v0.4.1: Removed old elements and added nginx conf
    * Removed src and non-public folders.
    * Added nginx confg.

--- src/scripts/server.js
+++
@@ -1,7 +0,0 @@
-function getLocalIP() {
-    let ipAddr = '172.26.189.77'
-    document.getElementById('ipAddr').textContent = ipAddr;
-    return ipAddr
-}
-
-console.log('Your IP Address is:', getLocalIP());
